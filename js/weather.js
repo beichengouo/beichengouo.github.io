d5c5f7bb51f55521c8b4d2774a8779f0a9cc42b7
beichengouo <zhangbaicheng44@gmail.com>
2023-05-11 16:49:34 +0800

    Site updated: 2023-05-11 16:49:34

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "15px 10px 10px 20px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "1f7ea05528334fa9995ae0f6050dfaa7"
+  }
+}
