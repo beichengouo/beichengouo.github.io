d7c2c561fd14ca20c92104ec329482600ec649e7
beicheng <1127090694@qq.com>
2026-03-27 19:41:03 +0800

    Restore to 2024-04-10 version

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "borderRadius": "5",
+    "horizontal": "left",
+    "city": "CN101240110",
+    "key": "e297d9d15fc849eeae45e7b3f6072828"
+  }
+}
